3c0ff3dc92097a92ee39f844d35bb504372b46b2
VladimirKoshovec <koshavladmir@gmail.com>
2022-11-09 23:06:47 +0200

    add_lazyload

--- js/skript.js
+++ js/skript.js
@@ -1,3 +1,15 @@
-const imgS = document.addEventListener("load",
-    document.querySelectorAll('img').forEach((im)=>{im.classList.add('lazyload')})
-);
+const imgS = document.addEventListener("load", showSite())
+
+function showSite () {
+    document.querySelectorAll('img').forEach((im)=>{im.classList.add('lazyload')});
+    setTimeout(loadhtml, 3000);
+    setTimeout(delloadhtml, 4000);
+}
+
+function loadhtml () {
+    document.querySelector('.loader').classList.add('loader-close')
+}
+
+function delloadhtml () {
+    document.querySelector('.loader').classList.add('loader-deley')
+}
