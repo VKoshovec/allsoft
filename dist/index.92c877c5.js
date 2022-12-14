251f66d9672a2fe05afecd781186876b2ecf1e70
VladimirKoshovec <koshavladmir@gmail.com>
2022-12-14 13:16:09 +0200

    add

---
+++ dist/index.92c877c5.js
@@ -0,0 +1,10 @@
+const ref = {
+    minimenu: document.querySelector(".menu__mini"),
+    openModalBtn: document.querySelector("[menu-mobile-open]")
+};
+const showMini = ()=>{
+    ref.minimenu.classList.add(".show");
+};
+ref.openModalBtn.addEventListener("click");
+
+//# sourceMappingURL=index.92c877c5.js.map
