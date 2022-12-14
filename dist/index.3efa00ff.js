cae202633b7cb6f4929f6d0db6f8b1abbc880fb2
VladimirKoshovec <koshavladmir@gmail.com>
2022-12-14 13:18:23 +0200

    rec

--- dist/index.3efa00ff.js
+++ dist/index.3efa00ff.js
@@ -532,18 +532,20 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"5zrwa":[function(require,module,exports) {
-// const imgS = document.addEventListener("load", showSite())
-// function showSite () {
-//     document.querySelectorAll('img').forEach((im)=>{im.classList.add('lazyload')});
-//     setTimeout(loadhtml, 2000);
-//     setTimeout(delloadhtml, 4000);
-// }
-// function loadhtml () {
-//     document.querySelector('.loader').classList.add('loader-close')
-// }
-// function delloadhtml () {
-//     document.querySelector('.loader').classList.add('loader-deley')
-// }
+const imgS = document.addEventListener("load", showSite());
+function showSite() {
+    document.querySelectorAll("img").forEach((im)=>{
+        im.classList.add("lazyload");
+    });
+    setTimeout(loadhtml, 3000);
+    setTimeout(delloadhtml, 4000);
+}
+function loadhtml() {
+    document.querySelector(".loader").classList.add("loader-close");
+}
+function delloadhtml() {
+    document.querySelector(".loader").classList.add("loader-deley");
+}
 // menu
 const ref = {
     minimenu: document.querySelector(".menu__mini"),
--- src/js/skript.js
+++ src/js/skript.js
@@ -1,18 +1,18 @@
-// const imgS = document.addEventListener("load", showSite())
+const imgS = document.addEventListener("load", showSite())
 
-// function showSite () {
-//     document.querySelectorAll('img').forEach((im)=>{im.classList.add('lazyload')});
-//     setTimeout(loadhtml, 2000);
-//     setTimeout(delloadhtml, 4000);
-// }
+function showSite () {
+    document.querySelectorAll('img').forEach((im)=>{im.classList.add('lazyload')});
+    setTimeout(loadhtml, 3000);
+    setTimeout(delloadhtml, 4000);
+}
 
-// function loadhtml () {
-//     document.querySelector('.loader').classList.add('loader-close')
-// }
+function loadhtml () {
+    document.querySelector('.loader').classList.add('loader-close')
+}
 
-// function delloadhtml () {
-//     document.querySelector('.loader').classList.add('loader-deley')
-// }
+function delloadhtml () {
+    document.querySelector('.loader').classList.add('loader-deley')
+}
 
 // menu
 const ref = {
